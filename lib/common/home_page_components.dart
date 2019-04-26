beead066b49f75b045b87e6e653f882504722cd3
abdulsamad aliyu <abdulsamadaliyu17@gmail.com>
2019-04-26 17:40:02 +0300

    Added one corner radius and plus button

--- lib/common/home_page_components.dart
+++ lib/common/home_page_components.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pocketnote/styleguide/colors.dart';
 
 class HomePageComponents extends StatefulWidget {
   @override
@@ -13,22 +14,61 @@ class HomePageComponentsState extends State<HomePageComponents> {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     // TODO: implement build
     return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return [
-          SliverAppBar(
-            expandedHeight: 300,
-            floating: true,
-            pinned: false,
-            snap: false,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Center(child: Text('hey'),),
-            ),
-          )
-        ];
-      },
-      body: Container(color: Colors.white,child: Center(child: Text('hi'),),),
-    );
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: 300,
+              floating: true,
+              pinned: false,
+              snap: false,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Center(
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 40,
+                        top: 60,
+                        child: InkWell(
+                            child: Container(
+                              padding: EdgeInsets.all(12),
+                          child: Image.asset("assets/images/plus.png", fit: BoxFit.contain,),
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: primaryColor),
+                        )),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ];
+        },
+        body: Container(
+            child: Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Center(
+                  child: Text('hi'),
+                ),
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft:
+                              Radius.elliptical(width * 0.42, width * 0.52))),
+                )),
+            decoration: ShapeDecoration(
+              shadows: [
+                // BoxShadow(blurRadius: 10, color: shadowColor, spreadRadius: 0.5)
+              ],
+              color: primaryColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.only(topLeft: Radius.circular(width * 0.4))),
+            )));
   }
 }
--- lib/styleguide/colors.dart
+++ lib/styleguide/colors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 
 final Color backgroundColor = Color(0xFF262374);
 final Color primaryColor = Color(0xFF37348f);
+final Color shadowColor = Color(0x33000000);
